87290a43e1baff504e6d09096ed09a58f445c538
a01177702 <a01177702@tec.mx>
2023-08-29 09:40:50 -0600

    Qlearning matrix and rewards actualization

--- main.py
+++ main.py
@@ -1,170 +1,150 @@
 # import libraries
 import numpy as np
-import matplotlib.pyplot as plt
 
 # define the size of the field
-field_rows = 10
-field_cols = 10
+environment_rows = 5
+environment_columns = 5
 
-# Q-values for each state and action pair: Q(s, a)
-#   The third value is "action" dimension.
-#   The value of each (state, action) pair is initialized to 0.
-q_values = np.zeros((field_rows, field_cols, 5))
-
-# actions: 0 = wait, 1 = up, 2 = right, 3 = down, 4 = left
-actions = ["wait", "up", "right", "down", "left"]
-
-# holds the rewards for each state
-rewards = np.full((field_rows, field_cols), 20)
+#define actions
+#            0       1       2       3
+actions = ['up', 'right', 'down', 'left']
 
 
-# function that determines if the robot has finished
-def has_finished():  # TODO: cambiar esto
-    return np.count_nonzero(rewards == 20) <= 0
+#//////////////////////////////////////////////////////
+#HARVESTED AREA INFO
 
 
-def claim_reward(row, col, action):
-    if has_finished():  # if the robot has finished, give it a big reward
-        return 4000
+#initialization
+rewards = np.full((environment_rows, environment_columns), -100.)
+rewards[2, 2] = 100.
 
-    if action == 0:  # lazy penalty
-        action_reward = -2
-    else:  # reward for moving
-        action_reward = -1  # move penalty
-        action_reward += rewards[row, col]
-        rewards[row, col] = 0
-    return action_reward
+reward_row, reward_col = np.where(rewards == 100)
+# Update values to the right of the reward element to -1
+for i in range(len(reward_row)):
+    row = reward_row[i]
+    col = reward_col[i]
+    for c in range(col + 1, environment_columns):
+        rewards[row, c] = -1
 
+# Update values below the reward column to -1
+for i in range(len(reward_row)):
+    row = reward_row[i]
+    col = reward_col[i]
+    for r in range(row + 1, environment_rows):
+        for c in range(environment_columns):
+            rewards[r, c] = -1
 
-# get a random starting location for the robot
+# Print modified rewards matrix
+for row in rewards:
+    print(row)
+    
+    
+#///////////////////////////////////////////////
+#DEFINE HELPER FUNCTIONS   
+    
+    
+#define a function that determines if the specified location is a terminal state
+def is_terminal_state(current_row_index, current_column_index):
+  #if the reward for this location is -1, then it is not a terminal state (i.e., it is a 'white square')
+  if rewards[current_row_index, current_column_index] == -1.:
+    return False
+  else:
+    return True
+
+#define a function that will choose a random, non-terminal starting location
 def get_starting_location():
-    # it can be any row
-    initial_row = np.random.randint(field_rows)
+  #get a random row and column index
+  current_row_index = np.random.randint(environment_rows)
+  current_column_index = np.random.randint(environment_columns)
+  #continue choosing random row and column indexes until a non-terminal state is identified
+  #(i.e., until the chosen state is a 'white square').
+  while is_terminal_state(current_row_index, current_column_index):
+    current_row_index = np.random.randint(environment_rows)
+    current_column_index = np.random.randint(environment_columns)
+  return current_row_index, current_column_index
 
-    # start on the left or right side
-    possible_inital_cols = [0, field_cols - 1]
-    initial_col = possible_inital_cols[np.random.randint(len(possible_inital_cols))]
+#define an epsilon greedy algorithm that will choose which action to take next (i.e., where to move next)
+def get_next_action(current_row_index, current_column_index, epsilon):
+  #if a randomly chosen value between 0 and 1 is less than epsilon,
+  #then choose the most promising value from the Q-table for this state.
+  if np.random.random() < epsilon:
+    return np.argmax(q_values[current_row_index, current_column_index])
+  else: #choose a random action
+    return np.random.randint(4)
 
-    return initial_row, initial_col
+#define a function that will get the next location based on the chosen action
+def get_next_location(current_row_index, current_column_index, action_index):
+  new_row_index = current_row_index
+  new_column_index = current_column_index
+  if actions[action_index] == 'up' and current_row_index > 0:
+    new_row_index -= 1
+  elif actions[action_index] == 'right' and current_column_index < environment_columns - 1:
+    new_column_index += 1
+  elif actions[action_index] == 'down' and current_row_index < environment_rows - 1:
+    new_row_index += 1
+  elif actions[action_index] == 'left' and current_column_index > 0:
+    new_column_index -= 1
+  return new_row_index, new_column_index
 
-
-# epsilon greedy alorithm that will choose which action to take next
-def get_next_action(current_row, current_col, epsilon):
-    if np.random.random() < epsilon:  # choose best action
-        return np.argmax(q_values[current_row, current_col])
-    else:  # Random action
-        return np.random.randint(len(actions))
-
-
-# perform the action
-def perform_action(current_row, current_col, action):
-    new_row = current_row
-    new_col = current_col
-    action_reward = 0
-
-    if actions[action] == "wait":
-        pass
-    elif actions[action] == "down":
-        if current_row < field_rows - 1:
-            new_row += 1
-        else:
-            action_reward = -1000
-    elif actions[action] == "right":
-        if current_col < field_cols - 1:
-            new_col += 1
-        else:
-            action_reward = -1000
-    elif actions[action] == "up":
-        if current_row > 0:
-            new_row -= 1
-        else:
-            action_reward = -1000
-    elif actions[action] == "left":
-        if current_col > 0:
-            new_col -= 1
-        else:
-            action_reward = -1000
-
-    return new_row, new_col, action_reward
-
-
-
-def initialize_environment():
-    initial_rewards = np.full((field_rows, field_cols), 20)
-    return initial_rewards
+#Define a function that will get the shortest path between any location within the warehouse that
+#the robot is allowed to travel and the item packaging location.
+def get_shortest_path(start_row_index, start_column_index):
+  #return immediately if this is an invalid starting location
+  if is_terminal_state(start_row_index, start_column_index):
+    return []
+  else: #if this is a 'legal' starting location
+    current_row_index, current_column_index = start_row_index, start_column_index
+    shortest_path = []
+    shortest_path.append([current_row_index, current_column_index])
+    #continue moving along the path until we reach the goal (i.e., the item packaging location)
+    while not is_terminal_state(current_row_index, current_column_index):
+      #get the best action to take
+      action_index = get_next_action(current_row_index, current_column_index, 1.)
+      #move to the next location on the path, and add the new location to the list
+      current_row_index, current_column_index = get_next_location(current_row_index, current_column_index, action_index)
+      shortest_path.append([current_row_index, current_column_index])
+      
 
 
-# get the best path
-def get_best_path(starting_row, starting_col):
-    current_row, current_col = starting_row, starting_col
+#///////////////////////////////////////////////////
+#TRAIN THE AI AGENT USING Q-LEARNING
+      
+      
+      
+      #define training parameters
+epsilon = 0.9 #the percentage of time when we should take the best action (instead of a random action)
+discount_factor = 0.9 #discount factor for future rewards
+learning_rate = 0.9 #the rate at which the AI agent should learn
 
-    best_path = []
-    best_path.append([current_row, current_col])
-
-    step = 0
-    while not has_finished():
-        step += 1
-        # get the best action to take
-        action = get_next_action(current_row, current_col, 1)
-        current_row, current_col, reward = perform_action(current_row, current_col, action)
-        rewards[current_row, current_col] = 0
-        best_path.append([current_row, current_col])
-        print(rewards)
-
-    return best_path
-
-
-# training parameters
-EPSILON = 0.9  # the percentage of time when we should take the best action (instead of a random action)
-DISCOUNT_FACTOR = 0.9  # discount factor for future rewards
-LEARNING_RATE = 0.9  # the rate at which the AI agent should learn
-
-
-# for plotting metrics
-steps_per_episode = []
-
-
+#run through 1000 training episodes
 for episode in range(1000):
-    rewards = initialize_environment()
-    row, col = get_starting_location()
-    rewards[row, col] = 0
+  #get the starting location for this episode
+  row_index, column_index = get_starting_location()
 
-    steps = 0
+  #continue taking actions (i.e., moving) until we reach a terminal state
+  #(i.e., until we reach the item packaging area or crash into an item storage location)
+  while not is_terminal_state(row_index, column_index):
+    #choose which action to take (i.e., where to move next)
+    action_index = get_next_action(row_index, column_index, epsilon)
 
-    while not has_finished():
-        steps += 1
-        action = get_next_action(row, col, EPSILON)
+    #perform the chosen action, and transition to the next state (i.e., move to the next location)
+    old_row_index, old_column_index = row_index, column_index #store the old row and column indexes
+    row_index, column_index = get_next_location(row_index, column_index, action_index)
 
-        old_row, old_col = row, col
-        row, col, reward = perform_action(row, col, action)
+    #receive the reward for moving to the new state, and calculate the temporal difference
+    reward = rewards[row_index, column_index]
+    old_q_value = q_values[old_row_index, old_column_index, action_index]
+    temporal_difference = reward + (discount_factor * np.max(q_values[row_index, column_index])) - old_q_value
 
-        # receive the reward for moving the new state and calculate the temporal difference
-        reward += claim_reward(row, col, action)
-        old_q_value = q_values[old_row, old_col, action]
-        temporal_difference = reward + (DISCOUNT_FACTOR * np.max(q_values[row, col])) - old_q_value
+    #update the Q-value for the previous state and action pair
+    new_q_value = old_q_value + (learning_rate * temporal_difference)
+    q_values[old_row_index, old_column_index, action_index] = new_q_value
 
-        # update the q-value for the previous state and action pair
-        new_q_value = old_q_value + (LEARNING_RATE * temporal_difference)
-        q_values[old_row, old_col, action] = new_q_value
-
-    steps_per_episode.append(steps)
-    print("Training episode: {}".format(episode) + " - Steps: {}".format(steps))
-
-print("training complete!")
+print('Training complete!')
 
 
-# Crear la gráfica
-plt.plot(range(1000), steps_per_episode)
-plt.xlabel("Episode")
-plt.ylabel("Number of Steps")
-plt.title("Number of Steps per Episode")
-plt.show()
+#///////////////////////////////////////////////////
+#GET SHORTEST PATH
 
 
-
-#display a few shortest paths
-rewards = initialize_environment()
-rewards[0, 0] = 0
-path = get_best_path(0,0)
-print(path)
-
+print(get_shortest_path(4, 4))
